93b6ab331ac22ff77487c68d59424de6f7710b59
Kamil <kamil.ryzek00@gmail.com>
2025-01-02 17:43:35 +0100

    blokada usunięcia jedynego produktu w transakcji

--- firm/src/components/EdycjaTransakcji.js
+++ firm/src/components/EdycjaTransakcji.js
@@ -255,12 +255,16 @@ const EdycjaTransakcji = () => {
               placeholder="Ilość"
               className="w-24 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-            <button
-              onClick={() => handleRemoveProduct(index)}
-              className="relative flex items-center justify-center w-10 h-10 rounded-full text-gray-500 hover:text-red-600 hover:bg-red-100 active:bg-red-200 transition focus:outline-none"
-            >
-              <MinusIcon className="w-5 h-5" />
-            </button>
+                <button
+                  onClick={() => handleRemoveProduct(index)}
+                  className={`relative flex items-center justify-center w-10 h-10 rounded-full transition focus:outline-none ${transaction.transactionProducts.length > 1
+                      ? "text-gray-500 hover:text-red-600 hover:bg-red-100 active:bg-red-200"
+                      : "text-gray-300 cursor-not-allowed"
+                    }`}
+                  disabled={transaction.transactionProducts.length <= 1}
+                >
+                  <MinusIcon className="w-5 h-5" />
+                </button>
           </div>
         ))}
         <button
